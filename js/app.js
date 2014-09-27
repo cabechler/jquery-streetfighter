c3617bd41a74a452d4e2122c114ed65334ffdfde
cabechler <cathybechler@gmail.com>
2014-09-27 11:38:34 -0400

    finishing off
    finished js coding for keydown/keyup, added directions, added background
    image and theme music.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
+
+
+playtheme();
+
+
   $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
+    $('.ryu-still, .ryu-cool, .ryu-throwing').hide();
     $('.ryu-ready').show();
   })
+
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
+
   .mousedown(function() {
     playHadouken();    
-    $('.ryu-ready').hide();
+    $('.ryu-ready, .ryu-cool .ryu-still').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     .animate(
-      {'left': '900px'},
+      {'left': '1000px'},
       500,
       function() {
         $(this).hide();
@@ -21,10 +28,25 @@ $(document).ready(function() {
       }
     );
   })
+
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+
+  $(document).keydown(function(e) {
+  if (e.keyCode == 88 ) {
+    $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-cool').show();
+  }
+})
+
+.keyup(function(e) {
+  if (e.keyCode == 88){
+    $('.ryu-still').show();
+    $('.ryu-cool').hide();
+  };
 });
 
 function playHadouken () {
@@ -32,3 +54,11 @@ function playHadouken () {
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
+function playtheme () {
+  $('#play-stage')[0].volume = 0.5;
+  $('#play-stage')[0].load();
+  $('#play-stage')[0].play();
+  }
+});
+
